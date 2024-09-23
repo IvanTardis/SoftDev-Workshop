357c93cab73d6f540e3dd68b1bb8da01300c09fa
Ivan Gontchar <ivan.tardis@outlook.com>
2024-09-23 10:59:11 -0400

    v3

--- 09_softserve/v3/app.py
+++ 09_softserve/v3/app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)                 #create instance of class Flask
 
 @app.route("/")                       #assign fxn to route
 def hello_world():
-    print("about to print __name__...")
-    print(__name__)                   #where will this go?
+    print("about to print __name__..."")
+    print(__name__)                   #where will this go? In terminal
     return "No hablo queso!"
 
 app.debug = True

c0b1dde78a0e2af3ed5e3bb2abf219bbad8ffed3
Ivan Gontchar <ivan.tardis@outlook.com>
2024-09-25 10:32:59 -0400

    cleaned up the heading

--- 09_softserve/app.py
+++ 09_softserve/app.py
@@ -13,13 +13,13 @@ app = Flask(__name__)
 @app.route("/")                 #assign fxn to route
 def hello_world():
     print(__name__)
-    return '''
-    # Ivan Gontchar<br>
-    # Fire Marshalls<br>
-    # SoftDev<br>
-    # K09 -- Putting it Together<br>
-    # 2024-09-24<br>
-    # time spent: 1 hour<br><br><b>''' + numbercruncher.maker()
+    return '''<h3>
+    Ivan Gontchar<br>
+    Fire Marshalls<br>
+    SoftDev<br>
+    K09 -- Putting it Together<br>
+    2024-09-24<br>
+    time spent: 1 hour</h3><br><br><b>''' + numbercruncher.maker()
 
 if __name__ == "__main__":      # true if this file NOT imported
     app.debug = True            # enable auto-reload upon code change

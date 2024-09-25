70d4ef3bc0ad8df4416206731bb23c8e4216e956
Ivan Gontchar <ivan.tardis@outlook.com>
2024-09-24 23:03:58 -0400

    adding what I didn't get to add in school because the lab machines froze

--- 09_softserve/app.py
+++ 09_softserve/app.py
@@ -6,4 +6,15 @@
 # time spent:
 
 from flask import Flask
+import numbercruncher
+
 app = Flask(__name__)
+
+@app.route("/")                 #assign fxn to route
+def hello_world():
+    print(__name__)
+    return numbercruncher.maker()
+
+if __name__ == "__main__":      # true if this file NOT imported
+    app.debug = True            # enable auto-reload upon code change
+    app.run()

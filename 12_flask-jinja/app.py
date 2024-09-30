32c1c3794ff6c59b3041faeddd16f354556550d7
Ivan Gontchar <ivan.tardis@outlook.com>
2024-09-30 10:33:56 -0400

    moved foo

--- 12_flask-jinja/app.py
+++ 12_flask-jinja/app.py
@@ -39,7 +39,7 @@ it look for a folder called "templates"?
 # Q0: What will happen if you remove render_template from the following statement?
 # (log prediction before executing...)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-from flask import Flask, render_template
+from flask import Flask#, render_template
 app = Flask(__name__)
 
 @app.route("/")
@@ -51,7 +51,7 @@ coll = [0,1,1,2,3,5,8]
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Q1: Can all of your teammates confidently predict the URL to use to load this page?
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-@app.route("/my_foist_template") 
+@app.route("/my_foist_template")
 def test_tmplt():
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Q2: What is the significance of each argument? Simplest, most concise answer best.
@@ -60,5 +60,5 @@ def test_tmplt():
 
 
 if __name__ == "__main__":
-    app.debug = True
+    app.debug = False
     app.run()

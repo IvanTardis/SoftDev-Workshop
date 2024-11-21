36f21d734338560430b0a3b07befb1d6c8f48518
Ivan Gontchar <ivan.tardis@outlook.com>
2024-11-21 10:24:26 -0500

    quickly added an explanation paragraph.. oops

--- 23_rest/app.py
+++ 23_rest/app.py
@@ -30,11 +30,12 @@ dict = json.loads(s)
 
 imgURL = dict['url']
 description = dict['title']
+explanation = dict['explanation']
 # print(imgURL)
 
 @app.route(("/") , methods=['GET', 'POST'])
 def home():
-    return render_template('main.html', imgURL = imgURL, alt = description)
+    return render_template('main.html', explanation=explanation, imgURL = imgURL, alt = description)
 
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
